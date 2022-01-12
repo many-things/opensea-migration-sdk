07d76c5668299d950a7af646a014b63f867496ce
Junho Yeo <i@junho.io>
2022-01-12 20:25:56 +0900

    Remove unused comments

--- src/playground.ts
+++ src/playground.ts
@@ -7,9 +7,6 @@ const run = async () => {
     '0x8ee55f30b24e42827cb4aad0b75454b92b99f106',
   )
 
-  // const clonex = assets['0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b']
-  // console.log(clonex)
-
   fs.writeFileSync('./playground.json', JSON.stringify(assets, null, 2))
 }
 run()
