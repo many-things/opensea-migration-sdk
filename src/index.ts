1c64ac67468ce8f27e8fa451477939697dfbde45
Junho Yeo <i@junho.io>
2022-01-12 20:25:23 +0900

    Fetch orders made by account & order assets by sale price

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ type Address = string
 type UnixTimestamp = number
 
 type OpenseaAsset = {
+  token_id: string
   asset_contract: {
     address: Address
   }
@@ -39,7 +40,7 @@ type InternalAsset = any
 type InternalOrder = any
 
 export const getAssetsByAccountAddress = async (
-  address: string,
+  address: Address,
 ): Promise<InternalAsset> => {
   const url = `https://api.opensea.io/api/v1/assets`
   const {
@@ -47,7 +48,11 @@ export const getAssetsByAccountAddress = async (
   } = await axios.get(
     QueryString.stringifyUrl({
       url,
-      query: { owner: address, limit: 50 },
+      query: {
+        owner: address,
+        limit: 50,
+        order_by: 'sale_price',
+      },
     }),
   )
   return assets
@@ -55,6 +60,7 @@ export const getAssetsByAccountAddress = async (
 
 export const getOrdersByAssetContractAddress = async (
   assetContractAddress: Address,
+  tokenId: string,
 ): Promise<InternalOrder[]> => {
   const url = `https://api.opensea.io/wyvern/v1/orders`
   const {
@@ -66,7 +72,48 @@ export const getOrdersByAssetContractAddress = async (
         asset_contract_address: assetContractAddress,
         bundled: false,
         include_bundled: false,
-        token_id: '8694',
+        limit: 20,
+        offset: 0,
+        token_id: tokenId,
+        order_by: 'created_date',
+        order_direction: 'desc',
+      },
+    }),
+    {
+      headers: {
+        'X-API-KEY': 'bea970cbbdae445a9f01b827f9ac227e',
+      },
+    },
+  )
+  return orders.map((order: OpenseaOrder) => ({
+    id: order.id,
+    createdAt: new Date(order.listing_time * 1_000),
+    expireAt: new Date(order.expiration_time * 1_000),
+    maker: order.maker.address,
+    currentPrice: order.current_price,
+    currentBounty: order.current_bounty,
+    bountyMultiple: order.bounty_multiple,
+    quantity: parseInt(order.quantity),
+
+    cancelled: order.cancelled,
+    paymentToken: order.payment_token,
+    markedInvalid: order.marked_invalid,
+  }))
+}
+
+export const getOrdersByAccountAddress = async (
+  address: Address,
+): Promise<InternalOrder[]> => {
+  const url = `https://api.opensea.io/wyvern/v1/orders`
+  const {
+    data: { orders },
+  } = await axios.get(
+    QueryString.stringifyUrl({
+      url,
+      query: {
+        maker: address,
+        bundled: false,
+        include_bundled: false,
         limit: 20,
         offset: 0,
         order_by: 'created_date',
@@ -102,27 +149,43 @@ type AssetRecord = Record<
     orders: InternalOrder[]
   }
 >
+
+type LookupInformation = {
+  orders: InternalOrder[]
+  assets: AssetRecord
+}
 export const lookupAccountAddress = async (
   address: Address,
-): Promise<AssetRecord> => {
-  let record: Record<Address, InternalOrder> = {}
+  maxAssets: number = 10,
+): Promise<LookupInformation> => {
+  let assetRecord: Record<Address, InternalOrder> = {}
   const assets = await getAssetsByAccountAddress(address)
 
   await Promise.all(
-    assets.map((asset: OpenseaAsset) => {
+    assets.slice(0, maxAssets).map((asset: OpenseaAsset) => {
       return new Promise<void>((resolve) => {
+        const tokenId = asset.token_id
         const assetContractAddress = asset.asset_contract.address
-        getOrdersByAssetContractAddress(assetContractAddress).then((orders) => {
-          record[assetContractAddress] = {
-            ...record[assetContractAddress],
-            asset,
-            orders,
-          }
-          resolve()
-        })
+        getOrdersByAssetContractAddress(assetContractAddress, tokenId)
+          .then((orders) => {
+            assetRecord[assetContractAddress] = {
+              ...assetRecord[assetContractAddress],
+              asset,
+              orders,
+            }
+            resolve()
+          })
+          .catch((error) => console.log(error.message))
       })
     }),
   )
 
-  return record
+  const orders = await getOrdersByAccountAddress(address).catch((error) => {
+    console.log(error.message)
+    return []
+  })
+  return {
+    orders,
+    assets: assetRecord,
+  }
 }
--- src/playground.ts
+++ src/playground.ts
@@ -4,12 +4,12 @@ import { lookupAccountAddress } from './'
 
 const run = async () => {
   const assets = await lookupAccountAddress(
-    '0x4a003f0a2c52e37138eb646aB4E669C4A84C1001',
+    '0x8ee55f30b24e42827cb4aad0b75454b92b99f106',
   )
 
-  const clonex = assets['0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b']
-  console.log(clonex)
+  // const clonex = assets['0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b']
+  // console.log(clonex)
 
-  fs.writeFileSync('./playground.json', JSON.stringify(clonex, null, 4))
+  fs.writeFileSync('./playground.json', JSON.stringify(assets, null, 2))
 }
 run()
